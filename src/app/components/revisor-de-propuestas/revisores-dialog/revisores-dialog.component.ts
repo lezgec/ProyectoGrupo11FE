80c8d2ed963b1fc83c70a87da66bdf10056fbcb7
Luis Zamora <zamgarluer@gmail.com>
2025-01-29 08:31:36 -0500

    updates

---
+++ src/app/components/revisor-de-propuestas/revisores-dialog/revisores-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-revisores-dialog',
+  templateUrl: './revisores-dialog.component.html',
+  styleUrls: ['./revisores-dialog.component.css']
+})
+export class RevisoresDialogComponent {
+
+}
