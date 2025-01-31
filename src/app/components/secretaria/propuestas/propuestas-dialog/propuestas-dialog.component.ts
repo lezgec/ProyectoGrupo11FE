4c8b65b5d222c79d20a1be1f5c584c5ea5c2fc4f
Luis Zamora <78463738+lezgec@users.noreply.github.com>
2025-01-30 23:48:23 -0500

    Actualizaciones

--- src/app/components/secretaria/propuestas/propuestas-dialog/propuestas-dialog.component.ts
+++ src/app/components/secretaria/propuestas/propuestas-dialog/propuestas-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
+import { IAlumno } from 'src/app/models/alumno.model';
 @Component({
   selector: 'app-propuestas-dialog',
   templateUrl: './propuestas-dialog.component.html',
@@ -8,11 +8,13 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 })
 export class PropuestasDialogComponent {
   // Lista de alumnos disponibles
-  alumnos = [
-    { id: 1, nombre: 'Alumno 1' },
-    { id: 2, nombre: 'Alumno 2' },
-    { id: 3, nombre: 'Alumno 3' }
-  ];
+ alumnos: IAlumno[] = [
+       { id: 1, cedula: '1102233445', nombre: 'Juan', apellido: 'Pérez', telefono: '0991112233', correo: 'juan@example.com' },
+       { id: 2, cedula: '1105544332', nombre: 'María', apellido: 'López', telefono: '0993322114', correo: 'maria@example.com' },
+       { id: 3, cedula: '1109988776', nombre: 'Carlos', apellido: 'Ramírez', telefono: '0995566778', correo: 'carlos@example.com' },
+       { id: 4, cedula: '1108877665', nombre: 'Ana', apellido: 'Fernández', telefono: '0996677889', correo: 'ana@example.com' },
+       { id: 5, cedula: '1106655443', nombre: 'Luis', apellido: 'Torres', telefono: '0997788991', correo: 'luis@example.com' }
+     ];
 
   constructor(
     public dialogRef: MatDialogRef<PropuestasDialogComponent>,
